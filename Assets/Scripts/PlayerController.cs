e7618e6ffb5eb7019b16074af3fa9b69ed6249c9
cec264 <cec264@scarletmail.rutgers.edu>
2026-01-24 11:12:55 -0500

    Updated colors, UI name, and win condition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -51,8 +51,8 @@ public class PlayerController : MonoBehaviour {
 	// store a reference to that collider in a variable named 'other'..
 	void OnTriggerEnter(Collider other) 
 	{
-		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
-		if (other.gameObject.CompareTag ("Pick Up"))
+		// ..and if the game object we intersect has the tag 'PickUp' assigned to it..
+		if (other.gameObject.CompareTag ("PickUp"))
 		{
 			// Make the other game object (the pick up) inactive, to make it disappear
 			other.gameObject.SetActive (false);
@@ -71,8 +71,8 @@ public class PlayerController : MonoBehaviour {
 		// Update the text field of our 'countText' variable
 		countText.text = "Count: " + count.ToString ();
 
-		// Check if our 'count' is equal to or exceeded 12
-		if (count >= 12) 
+		// Check if our 'count' is equal to or exceeded 9
+		if (count >= 9) 
 		{
 			// Set the text value of our 'winText'
 			winText.text = "You Win!";
